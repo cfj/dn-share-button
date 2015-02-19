21cdfc1fef6b8dd95be9e82d1dfbbe51fdc0693d
Jonathan Svärdén <jonathan.svarden@gmail.com>
2015-02-19 21:29:31 +0100

    Fix errors in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('minify', function() {
-    return gulp.src('dn-button.js'))
+    return gulp.src('dn-button.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dn-button.min.js');
+    .pipe(rename('dn-button.min.js'))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['minify']);
